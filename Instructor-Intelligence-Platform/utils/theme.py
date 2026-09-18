ba40417ed8d40156167ef4899a864130a90202f3
anushka123jet-cloud <anushka.123jet@gmail.com>
2026-09-18 23:39:02 +0530

    Update load_css to use absolute path for style.css
    Refactor load_css function to use absolute path for CSS file.

--- Instructor-Intelligence-Platform/utils/theme.py
+++ Instructor-Intelligence-Platform/utils/theme.py
@@ -1,11 +1,17 @@
+import streamlit as st
+from pathlib import Path
+
+
 def load_css():
+    # Project root directory
+    BASE_DIR = Path(__file__).resolve().parent.parent
 
-    with open("assets/css/style.css") as f:
+    # Absolute path to CSS file
+    css_path = BASE_DIR / "assets" / "css" / "style.css"
 
-        import streamlit as st
-
+    # Load CSS
+    with open(css_path, "r", encoding="utf-8") as f:
         st.markdown(
             f"<style>{f.read()}</style>",
             unsafe_allow_html=True
         )
-
